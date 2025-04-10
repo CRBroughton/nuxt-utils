c3df8cee5fd245025eeb0279993477210aba654a
CRBroughton <crbroughton@posteo.uk>
2025-04-10 08:43:38 +0100

    fix: :label: fix types for exporting - see https://github.com/nuxt/module-builder/issues/141

--- playground/app.vue
+++ playground/app.vue
@@ -1,10 +1,23 @@
 <script setup lang="ts">
-const { data, status } = await useParallelFetch('blah', {
-  firstTen: `https://dummyjson.com/products?limit=10&skip=${0 * 10}`,
-  secondTen: `https://dummyjson.com/products?limit=10&skip=${1 * 10}`,
+const test = createUseParallelFetch($fetch, {
+  baseURL: 'https://dummyjson.com',
+  onRequest({ options }) {
+    console.log('onRequest', options)
+  },
+  onResponse({ response }) {
+    console.log('onResponse', response)
+  },
+  onResponseError({ error }) {
+    console.log('onResponseError', error)
+  },
+})
+
+const { data, status } = await test('blah', {
+  firstTen: `/products?limit=10&skip=${0 * 10}`,
+  secondTen: `/products?limit=10&skip=${1 * 10}`,
 }, {
   server: false,
-})
+}, {})
 </script>
 
 <template>
@@ -12,10 +25,16 @@ const { data, status } = await useParallelFetch('blah', {
     loading...
   </div>
   <div v-else>
-    <div v-for="product in data?.firstTen.products">
+    <div
+      v-for="product in data?.firstTen.products"
+      :key="product.id"
+    >
       {{ product.id }}
     </div>
-    <div v-for="product in data?.secondTen.products">
+    <div
+      v-for="product in data?.secondTen.products"
+      :key="product.id"
+    >
       {{ product.id }}
     </div>
   </div>
--- src/runtime/composables/useParallelFetch.ts
+++ src/runtime/composables/useParallelFetch.ts
@@ -1,16 +1,17 @@
-import type { AsyncDataOptions } from 'nuxt/app'
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import type { AsyncData, AsyncDataOptions, NuxtError } from 'nuxt/app'
 import { useAsyncData } from 'nuxt/app'
-import { $fetch, type FetchOptions } from 'ofetch'
 
 /**
  * Type for any function that returns a Promise
  */
 type FetchFunction<T> = () => Promise<T>
 
+export type PickFrom<T, K extends Array<string>> = T extends Array<any> ? T : T extends Record<string, any> ? keyof T extends K[number] ? T : K[number] extends never ? T : Pick<T, K[number]> : T
+export type KeysOf<T> = Array<T extends T ? keyof T extends string ? keyof T : never : never>
+
 /**
- * Fetches multiple resources in parallel and combines them into a single result object. Expands on useAsyncData and accepts
- * the useAsyncData options object for additional configuration.
- *
+ * Fetches multiple resources in parallel and combines them into a single result object. Expands on useAsyncData and accepts the useAsyncData options object for additional configuration.
  * @template T - The combined result type, a record with keys corresponding to different resources
  * @param key - Unique key for caching with useAsyncData
  * @param fetchMap - Object where each key corresponds to a function that fetches a resource
@@ -21,7 +22,7 @@ export function useManualParallelFetch<T extends Record<string, unknown>>(
   key: string,
   fetchMap: { [K in keyof T]: FetchFunction<T[K]> },
   options?: AsyncDataOptions<T>,
-) {
+): AsyncData<PickFrom<T, KeysOf<T>> | null, NuxtError<unknown> | null> {
   return useAsyncData<T>(key, async () => {
     try {
       const entries = Object.entries(fetchMap) as [keyof T, FetchFunction<unknown>][]
@@ -44,28 +45,40 @@ export function useManualParallelFetch<T extends Record<string, unknown>>(
 }
 
 /**
- * Fetches multiple URL endpoints in parallel and combines them into a single result object. Expands on both useAsyncData and $fetch and accepts
- * the useAsyncData and $fetch options object for additional configuration.
+ * Creates an instance of useParallelFetch with the given fetch function.
  *
- * @template T - The combined result type, a record with keys corresponding to different resources
- * @param key - Unique key for caching with useAsyncData
- * @param urlMap - Object where each key corresponds to a URL endpoint to fetch
- * @param asyncDataOptions - Options to pass to useAsyncData
+ * @param fetchFn - The fetch function to use for requests ($fetch),
  * @param fetchOptions - Options to pass to $fetch for all requests
- * @returns AsyncData object with combined results from all fetches
+ * @returns A configured useParallelFetch hook
  */
-export function useParallelFetch<T extends Record<string, unknown>>(
-  key: string,
-  urlMap: { [K in keyof T]: string },
-  asyncDataOptions?: AsyncDataOptions<T>,
+export function createUseParallelFetch(
+  fetchFn: typeof $fetch,
   fetchOptions?: FetchOptions,
 ) {
-  const fetchMap: { [K in keyof T]: FetchFunction<T[K]> } = Object.fromEntries(
-    Object.entries(urlMap).map(([k, url]) => [
-      k,
-      () => $fetch(url, fetchOptions) as Promise<T[typeof k & keyof T]>,
-    ]),
-  ) as { [K in keyof T]: FetchFunction<T[K]> }
+  /**
+   * Fetches multiple URL endpoints in parallel and combines them into a single result object. Expands on useAsyncData and accepts
+   * the useAsyncData options object for additional configuration.
+   *
+   * @template T - The combined result type, a record with keys corresponding to different resources
+   * @param key - Unique key for caching with useAsyncData
+   * @param urlMap - Object where each key corresponds to a URL endpoint to fetch
+   * @param asyncDataOptions - Options to pass to useAsyncData
+   * @returns AsyncData object with combined results from all fetches
+   */
+  return function useParallelFetch<T extends Record<string, unknown>>(
+    key: string,
+    urlMap: { [K in keyof T]: string },
+    asyncDataOptions?: AsyncDataOptions<T>,
+  ) {
+    const fetchMap: { [K in keyof T]: FetchFunction<T[K]> } = Object.fromEntries(
+      Object.entries(urlMap).map(([k, url]) => [
+        k,
+        () => fetchFn(url, fetchOptions) as Promise<T[typeof k & keyof T]>,
+      ]),
+    ) as { [K in keyof T]: FetchFunction<T[K]> }
 
-  return useManualParallelFetch<T>(key, fetchMap, asyncDataOptions)
+    return useManualParallelFetch<T>(key, fetchMap, asyncDataOptions)
+  }
 }
+
+type FetchOptions = Parameters<typeof $fetch>[1]
